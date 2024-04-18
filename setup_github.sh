fcce978eab74a2e12e296400f40cca4bd33fe3be
stevenmfowler <stevenfowler@live.com>
2024-04-18 16:56:33 -0400

    add setup_github.sh

--- setup_github.sh
+++ setup_github.sh
@@ -13,6 +13,7 @@ echo -e $YELLOW
 read -p "Enter your Git user.name: " username
 read -p "Enter your Git user.email: " email
 read -p "Enter your Git token: " token
+read -p "Enter your Git project: " project
 
 echo -e $PINK
 
@@ -22,8 +23,9 @@ echo -e "Set git user.name\n"
 git config --global user.email $email
 echo -e "Set git user.email\n"
 
-git remote set-url origin "https://$token@github.com/$username"
-echo -e "Setup github access using $token\n"
+git remote set-url origin "https://$token@github.com/$username/$project"
+echo -e "https://$token@github.com/$username/$project"
+echo -e "Setup github access to $project using $token for $username\n"
 
 git config --global init.default branch main
 echo -e $PINK"Set default branch\n"
