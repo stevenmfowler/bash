815cedcd657eca7f837a24006d1fe7b8cc32e531
stevenmfowler <stevenfowler@live.com>
2024-04-24 17:24:02 -0400

    adding set_lm_dx.sh

---
+++ set_lm_dx.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# Description:
+
+# Usage: create_lm_vm.sh
+
+# Author: Steven Fowler (https://github.com/stevenmfowler)
+# License: MIT (https://mit-license.org)
+
+# Date: 2024-04-24
+
+gsettings set org.cinnamon.desktop.interface font-name 'Sans 16'
+
+# Success message
+echo "Desktop fonts size adjusted."
+
+
+echo "You can make more changes, stand by..."
+sleep 3
+
+cinnamon-settings
+
+
+
